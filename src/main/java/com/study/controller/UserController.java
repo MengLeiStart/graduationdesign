f3fbddc10f0cf875774a55486303f9a1dd946ea9
MengLeiStart <728093780@qq.com>
2022-11-09 20:06:10 +0800

    完善验证码功能,重新写登录注册界面

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -3,17 +3,19 @@ import com.study.util.CheckCodeUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
 
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    @RequestMapping("/user")
+    @RequestMapping("/checkCode")
     public void checkCode(HttpServletResponse response, HttpSession session) throws IOException {
         ServletOutputStream os = response.getOutputStream();
         String code = CheckCodeUtil.outputVerifyImage(100,50,os,4);
-
     }
 }
