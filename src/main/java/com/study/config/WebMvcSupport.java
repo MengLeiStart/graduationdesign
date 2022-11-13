4162006ac205c2148f26d94dd574db44af728cc8
MengLeiStart <728093780@qq.com>
2022-11-13 21:59:12 +0800

    完善代码

--- src/main/java/com/study/config/WebMvcSupport.java
+++ src/main/java/com/study/config/WebMvcSupport.java
@@ -1,14 +1,14 @@
 package com.study.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcSupport extends WebMvcConfigurationSupport {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //配置虚拟路径，解决浏览器无法直接访问本地文件的安全性问题
         registry.addResourceHandler("/img/**").addResourceLocations("file:D:/image/");
+        //给静态资源放行
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 
     }
